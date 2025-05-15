25f3e9e18f1f0d95e77a6ac71ae11cf3385a4c6b
codewitch-honey-crisis <gaztoast@gmail.com>
2025-05-15 08:11:21 -0700

    fixed example

--- examples/demo/src/main.cpp
+++ examples/demo/src/main.cpp
@@ -1,71 +1,69 @@
 #include <Arduino.h>
-#include <mpm_parser.h>
 
-const char* boundary ="---------------------------9051914041544843365972754266";
-const char* content = "-----------------------------9051914041544843365972754266\r\n"
-"Content-Disposition: form-data; name=\"text\"\r\n"
-"\r\n"
-"text default\r\n"
-"-----------------------------9051914041544843365972754266\r\n"
-"Content-Disposition: form-data; name=\"file1\"; filename=\"a.txt\"\r\n"
-"Content-Type: text/plain\r\n"
-"\r\n"
-"Data of a.txt.\r\n"
-"\r\n"
-"-----------------------------9051914041544843365972754266\r\n"
-"Content-Disposition: form-data; name=\"file2\"; filename=\"a.html\"\r\n"
-"Content-Type: text/html\r\n"
-"\r\n"
-"<!DOCTYPE html><title>Data of a.html.</title>\r\n"
-"\r\n"
-"-----------------------------9051914041544843365972754266--";
+#include "base64.h"
+static const char* base64_encoded = "aGVsbG8gd29ybGQtLQ==";
+static const char* base64_decoded = "hello world--";
 
-char buffer[1025];
 typedef struct {
     const char* input;
-} string_state_t;
+} string_buffer_t;
 static int string_read(void* state) {
-    string_state_t* ss = (string_state_t*)state;
-    if(ss->input==NULL) { return -1; }
-    if(!*ss->input) {
+    string_buffer_t* sb = (string_buffer_t*)state;
+    if (sb->input == NULL) {
         return -1;
     }
-    return *(ss->input++);
+    if (!*sb->input) {
+        return -1;
+    }
+    return *(sb->input++);
 }
 void setup() {
     Serial.begin(115200);
     Serial.println("booted");
-    mpm_context_t ctx;
-    string_state_t buf = {0};
-    buf.input = content;
-    mpm_init(boundary,0,string_read,&buf ,&ctx);
-    char buffer[1025];
-    size_t size = sizeof(buffer)-1;
-    mpm_node_t node;
+    string_buffer_t buf;
+    base64_context_t ctx;
+    
+    // encode
+    buf.input = base64_decoded;
+    char buffer[256];
+    size_t len;
+    int result;
+
+    base64_init(string_read, &buf, &ctx);
+    len = sizeof(buffer) - 1;
     do {
-        node = mpm_parse(&ctx, buffer, &size);
-        buffer[size] = '\0';
-        switch (node) {
-            case MPM_HEADER_NAME_PART:
-            case MPM_HEADER_VALUE_PART:
-            case MPM_CONTENT_PART:
-                fputs(buffer, stdout);
-                break;
-            case MPM_HEADER_NAME_END:
-                fputs(": ", stdout);                
-                break;
-            case MPM_HEADER_VALUE_END:
-                fputs("; ",stdout);
-                break;
-            case MPM_HEADER_END:
-                fputs(" (HEADER)\r\n", stdout);
-                break;
-            case MPM_CONTENT_END:
-                fputs("<END CONTENT>\r\n", stdout);
-                break;
-        }
-        size = sizeof(buffer) - 1;
-    } while (node > 0) ;
+        result = base64_encode(&ctx, (uint8_t*)buffer, &len);
+        buffer[len] = 0;
+        Serial.print(buffer);
+        len = sizeof(buffer) - 1;
+
+    } while (result > 0);
+    Serial.println("");
+    if (result < 0) {
+        Serial.println("error");
+    } else {
+        Serial.println("done");
+    }
+
+    // decode
+    buf.input = base64_encoded;
+    base64_init(string_read, &buf, &ctx);
+    len = sizeof(buffer) - 1;
+    do {
+        result = base64_decode(&ctx, (uint8_t*)buffer, &len);
+        buffer[len] = 0;
+        Serial.print(buffer);
+        len = sizeof(buffer) - 1;
+
+    } while (result > 0);
+    Serial.println("");
+    if (result < 0) {
+        Serial.println("error");
+    } else {
+        Serial.println("done");
+    }
+
+    return 0;
 }
 
 void loop() {
